c2ea6a955cf418190e270f5b437451d47ff5b912
Don Tay Xi Cheng <dontay0209@gmail.com>
2022-03-25 15:55:30 +0800

    Add type hint and change to os.environ

--- ingest.py
+++ ingest.py
@@ -7,10 +7,12 @@ from dateutil import parser
 from dotenv import load_dotenv
 from operator import itemgetter
 
-def get_req_handler(url):
+
+def get_req_handler(url: str) -> requests.Response:
     response = requests.get(url)
     if response.status_code != 200:
-        print("Non-OK status " + response.status_code + " from " + url, file=sys.stderr)
+        print("Non-OK status " + str(response.status_code) +
+              " from " + url, file=sys.stderr)
     return response
 
 
@@ -18,16 +20,16 @@ load_dotenv()
 
 # initialize AWS boto client
 session = boto3.Session(
-    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
-    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY")
+    aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"],
+    aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"]
 )
 
 # initialize S3 ServiceResource and bucket
 s3_resource = boto3.resource('s3')
-img_bucket = s3_resource.Bucket(os.getenv("BUCKET_NAME"))
+img_bucket = s3_resource.Bucket(os.environ["BUCKET_NAME"])
 
 # GET request to data API
-response = get_req_handler(os.getenv("IMAGE_API_URL"))
+response = get_req_handler(os.environ["IMAGE_API_URL"])
 
 res_json = response.json()
 if res_json['api_info']['status'] != 'healthy':
@@ -38,9 +40,11 @@ timestamp, cameras = itemgetter('timestamp', 'cameras')(item)
 iso_datetime = parser.parse(timestamp).strftime('%Y-%m-%dT%H%M%S')
 
 for camera in cameras:
-    image, location, camera_id, image_metadata = itemgetter('image', 'location', 'camera_id', 'image_metadata')(camera)
+    image, location, camera_id, image_metadata = itemgetter(
+        'image', 'location', 'camera_id', 'image_metadata')(camera)
     # sample filename format: cam-img/2022-03-21T235548_1002.jpg
-    file_name = os.getenv("CAM_IMG_DIR") + iso_datetime + '_' + camera_id + '.jpg'
+    file_name = os.environ["CAM_IMG_DIR"] + iso_datetime + \
+        '_' + camera_id + '.jpg'
     print(file_name)
     # get image stream
     img_resp = get_req_handler(image)
--- read_dl.py
+++ read_dl.py
@@ -3,6 +3,8 @@ import os
 
 from datetime import datetime
 from dotenv import load_dotenv
+from mypy_boto3_s3.service_resource import Bucket
+from typing import BinaryIO, Dict
 
 # Helper functions to get data from the various data lakes:
 # 1. Image from S3
@@ -11,37 +13,38 @@ from dotenv import load_dotenv
 today_date = datetime.today().date()
 
 
-# s3_bucket: S3 Service Resource type
+# s3_bucket: S3.Bucket
 # search_key: prefix key to search
-# returns a dictionary stucture: { filename: StreamingBody }
+# returns dictionary stucture: { filename: StreamingBody }
 # NB: use method read() on StreamingBody to convert to bytes
-def get_s3_images(s3_bucket, search_key):
-  obj_summaries = s3_bucket.objects.filter(
-      Prefix=search_key
-  )
-  res = {}
-  for summary in obj_summaries:
-    obj = summary.get()
-    res[summary.key] = obj['Body']
-  return res
+def get_s3_images(s3_bucket: Bucket, search_key: str) -> Dict[str, BinaryIO]:
+    obj_summaries = s3_bucket.objects.filter(
+        Prefix=search_key
+    )
+    res = {}
+    for summary in obj_summaries:
+        obj = summary.get()
+        res[summary.key] = obj['Body']
+    return res
+
 
 load_dotenv()
 
 # initialize AWS boto client
 session = boto3.Session(
-    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
-    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY")
+    aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"],
+    aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"]
 )
 
 # initialize S3 ServiceResource and bucket
 s3_resource = boto3.resource('s3')
-img_bucket = s3_resource.Bucket(os.getenv("BUCKET_NAME"))
+img_bucket = s3_resource.Bucket(os.environ["BUCKET_NAME"])
 
 # retrieve and download all of today's images to local file dir
-search_key = os.getenv("CAM_IMG_DIR") + str(today_date)
+search_key = os.environ["CAM_IMG_DIR"] + str(today_date)
 res = get_s3_images(img_bucket, search_key)
-if not os.path.exists(os.getenv("CAM_IMG_DIR")):
-    os.makedirs(os.getenv("CAM_IMG_DIR"))
+if not os.path.exists(os.environ["CAM_IMG_DIR"]):
+    os.makedirs(os.environ["CAM_IMG_DIR"])
 for filename, stream_data in res.items():
     with open(filename, 'wb') as f:
         f.write(stream_data.read())
