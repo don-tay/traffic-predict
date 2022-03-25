95e6f78275c6390060895d424631262540df131b
Don Tay Xi Cheng <dontay0209@gmail.com>
2022-03-25 18:42:20 +0800

    Refactor helper to separate file

--- ingest.py
+++ ingest.py
@@ -1,37 +1,29 @@
 import sys
 import boto3
 import os
-import requests
 
 from dateutil import parser
 from dotenv import load_dotenv
+from helper import get_req_handler, initBoto3Session
 from operator import itemgetter
 
-
-def get_req_handler(url: str) -> requests.Response:
-    response = requests.get(url)
-    if response.status_code != 200:
-        print(
-            "Non-OK status " + str(response.status_code) + " from " + url,
-            file=sys.stderr,
-        )
-    return response
-
-
+############ CONFIG INIT BOILERPLATE ############
+# load env vars
 load_dotenv()
 
-# initialize AWS boto client
-session = boto3.Session(
-    aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"],
-    aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"],
-)
+initBoto3Session()
 
-# initialize S3 ServiceResource and bucket
+# initialize S3 ServiceResource and bucket (init boto3 session first)
 s3_resource = boto3.resource("s3")
 img_bucket = s3_resource.Bucket(os.environ["BUCKET_NAME"])
 
+############ END OF CONFIG INIT BOILERPLATE ############
+
+CAM_IMG_DIR = os.environ["CAM_IMG_DIR"]
+IMAGE_API_URL = os.environ["IMAGE_API_URL"]
+
 # GET request to data API
-response = get_req_handler(os.environ["IMAGE_API_URL"])
+response = get_req_handler(IMAGE_API_URL)
 
 res_json = response.json()
 if res_json["api_info"]["status"] != "healthy":
@@ -46,12 +38,12 @@ for camera in cameras:
         "image", "location", "camera_id", "image_metadata"
     )(camera)
     # sample filename format: cam-img/2022-03-21T235548_1002.jpg
-    file_name = os.environ["CAM_IMG_DIR"] + iso_datetime + "_" + camera_id + ".jpg"
-    print(file_name)
+    file_name = CAM_IMG_DIR + iso_datetime + "_" + camera_id + ".jpg"
     # get image stream
     img_resp = get_req_handler(image)
     try:
         # upload image to S3
         img_bucket.put_object(Body=img_resp.content, Key=file_name)
+        print("Successfully uploaded image " + file_name + " to S3")
     except Exception:
-        print("Failed to upload image " + file_name)
+        print("Failed to upload image " + file_name, file=sys.stderr)
--- read_dl.py
+++ read_dl.py
@@ -1,11 +1,9 @@
 import boto3
 import os
 
-from botocore.response import StreamingBody
 from datetime import datetime
 from dotenv import load_dotenv
-from mypy_boto3_s3.service_resource import Bucket
-from typing import Dict
+from helper import get_s3_objs, initBoto3Session
 
 # Helper functions to get data from the various data lakes:
 # 1. Image object from S3
@@ -13,34 +11,25 @@ from typing import Dict
 
 today_date = datetime.today().date()
 
+############ CONFIG INIT BOILERPLATE ############
+# load env vars
 load_dotenv()
 
-# initialize AWS boto client
-session = boto3.Session(
-    aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"],
-    aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"],
-)
+initBoto3Session()
 
-# initialize S3 ServiceResource and bucket
+# initialize S3 ServiceResource and bucket (init boto3 session first)
 s3_resource = boto3.resource("s3")
 img_bucket = s3_resource.Bucket(os.environ["BUCKET_NAME"])
 
-# s3_bucket: S3.Bucket
-# search_key: prefix key to search
-# returns dictionary stucture: { filename: StreamingBody }
-# NB: use method read() on StreamingBody to convert to bytes
-def get_s3_objs(s3_bucket: Bucket, search_key: str) -> Dict[str, StreamingBody]:
-    obj_summaries = s3_bucket.objects.filter(Prefix=search_key)
+############ END OF CONFIG INIT BOILERPLATE ############
 
-    res = dict([(summary.key, summary.get()["Body"]) for summary in obj_summaries])
-    return res
-
+CAM_IMG_DIR = os.environ["CAM_IMG_DIR"]
 
 # retrieve and download all of today's images to local file dir
-search_key = os.environ["CAM_IMG_DIR"] + str(today_date)
+search_key = CAM_IMG_DIR + str(today_date)
 res = get_s3_objs(img_bucket, search_key)
-if not os.path.exists(os.environ["CAM_IMG_DIR"]):
-    os.makedirs(os.environ["CAM_IMG_DIR"])
+if not os.path.exists(CAM_IMG_DIR):
+    os.makedirs(CAM_IMG_DIR)
 for filename, stream_data in res.items():
     with open(filename, "wb") as f:
         f.write(stream_data.read())
