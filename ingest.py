34db9f6b0bb70744aa234baeb60963c07b35d50a
Don Tay Xi Cheng <dontay0209@gmail.com>
2022-03-25 17:39:40 +0800

    Add py linting black

--- ingest.py
+++ ingest.py
@@ -11,8 +11,10 @@ from operator import itemgetter
 def get_req_handler(url: str) -> requests.Response:
     response = requests.get(url)
     if response.status_code != 200:
-        print("Non-OK status " + str(response.status_code) +
-              " from " + url, file=sys.stderr)
+        print(
+            "Non-OK status " + str(response.status_code) + " from " + url,
+            file=sys.stderr,
+        )
     return response
 
 
@@ -21,30 +23,30 @@ load_dotenv()
 # initialize AWS boto client
 session = boto3.Session(
     aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"],
-    aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"]
+    aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"],
 )
 
 # initialize S3 ServiceResource and bucket
-s3_resource = boto3.resource('s3')
+s3_resource = boto3.resource("s3")
 img_bucket = s3_resource.Bucket(os.environ["BUCKET_NAME"])
 
 # GET request to data API
 response = get_req_handler(os.environ["IMAGE_API_URL"])
 
 res_json = response.json()
-if res_json['api_info']['status'] != 'healthy':
+if res_json["api_info"]["status"] != "healthy":
     print("Image API unhealthy", file=sys.stderr)
 
-item = res_json['items'][0]
-timestamp, cameras = itemgetter('timestamp', 'cameras')(item)
-iso_datetime = parser.parse(timestamp).strftime('%Y-%m-%dT%H%M%S')
+item = res_json["items"][0]
+timestamp, cameras = itemgetter("timestamp", "cameras")(item)
+iso_datetime = parser.parse(timestamp).strftime("%Y-%m-%dT%H%M%S")
 
 for camera in cameras:
     image, location, camera_id, image_metadata = itemgetter(
-        'image', 'location', 'camera_id', 'image_metadata')(camera)
+        "image", "location", "camera_id", "image_metadata"
+    )(camera)
     # sample filename format: cam-img/2022-03-21T235548_1002.jpg
-    file_name = os.environ["CAM_IMG_DIR"] + iso_datetime + \
-        '_' + camera_id + '.jpg'
+    file_name = os.environ["CAM_IMG_DIR"] + iso_datetime + "_" + camera_id + ".jpg"
     print(file_name)
     # get image stream
     img_resp = get_req_handler(image)
@@ -52,4 +54,4 @@ for camera in cameras:
         # upload image to S3
         img_bucket.put_object(Body=img_resp.content, Key=file_name)
     except Exception:
-        print('Failed to upload image ' + file_name)
+        print("Failed to upload image " + file_name)
--- read_dl.py
+++ read_dl.py
@@ -8,45 +8,41 @@ from mypy_boto3_s3.service_resource import Bucket
 from typing import Dict
 
 # Helper functions to get data from the various data lakes:
-# 1. Image from S3
+# 1. Image object from S3
 # 2. Weather data from RDS (TODO)
 
 today_date = datetime.today().date()
-
-
-# s3_bucket: S3.Bucket
-# search_key: prefix key to search
-# returns dictionary stucture: { filename: StreamingBody }
-# NB: use method read() on StreamingBody to convert to bytes
-def get_s3_images(s3_bucket: Bucket, search_key: str) -> Dict[str, StreamingBody]:
-    obj_summaries = s3_bucket.objects.filter(
-        Prefix=search_key
-    )
-
-    res = dict([(summary.key, summary.get()['Body'])
-               for summary in obj_summaries])
-    return res
-
 
 load_dotenv()
 
 # initialize AWS boto client
 session = boto3.Session(
     aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"],
-    aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"]
+    aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"],
 )
 
 # initialize S3 ServiceResource and bucket
-s3_resource = boto3.resource('s3')
+s3_resource = boto3.resource("s3")
 img_bucket = s3_resource.Bucket(os.environ["BUCKET_NAME"])
+
+# s3_bucket: S3.Bucket
+# search_key: prefix key to search
+# returns dictionary stucture: { filename: StreamingBody }
+# NB: use method read() on StreamingBody to convert to bytes
+def get_s3_objs(s3_bucket: Bucket, search_key: str) -> Dict[str, StreamingBody]:
+    obj_summaries = s3_bucket.objects.filter(Prefix=search_key)
+
+    res = dict([(summary.key, summary.get()["Body"]) for summary in obj_summaries])
+    return res
+
 
 # retrieve and download all of today's images to local file dir
 search_key = os.environ["CAM_IMG_DIR"] + str(today_date)
-res = get_s3_images(img_bucket, search_key)
+res = get_s3_objs(img_bucket, search_key)
 if not os.path.exists(os.environ["CAM_IMG_DIR"]):
     os.makedirs(os.environ["CAM_IMG_DIR"])
 for filename, stream_data in res.items():
-    with open(filename, 'wb') as f:
+    with open(filename, "wb") as f:
         f.write(stream_data.read())
 
-print('successfully retrieved all images for ' + str(today_date))
+print("successfully retrieved all images for " + str(today_date))
