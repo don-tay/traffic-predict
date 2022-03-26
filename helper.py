84d7ef20e098572c6852835767b33f2216bd64de
Retr0Metal98 <66635436+Retr0Metal98@users.noreply.github.com>
2022-03-27 01:16:16 +0800

    2-hr weather forecast ingestion, bucket save and merge files
    function to ingest 2-hr weather forecast data as CSVs
    
    code to output csv files to S3 bucket/local filesystem
    
    code to merge timestamped csv files and reupload to S3.
    
    job scheduling code added

--- helper.py
+++ helper.py
@@ -6,8 +6,18 @@ import sys
 from botocore.response import StreamingBody
 from mypy_boto3_s3.service_resource import Bucket
 from typing import Dict
+import pandas as pd
+from io import StringIO
 
 # Commonly used helper functions
+
+# Initialize boto3 session from env vars (ensure load_dotenv fn is called beforehand)
+def initBoto3Session() -> None:
+    boto3.Session(
+        aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"],
+        aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"],
+    )
+
 
 # s3_bucket: S3.Bucket
 # search_key: prefix key to search
@@ -34,9 +44,33 @@ def get_req_handler(url: str, params: dict) -> requests.Response:
     return response
 
 
-# Initialize boto3 session from env vars (ensure load_dotenv fn is called beforehand)
-def initBoto3Session() -> None:
-    boto3.Session(
-        aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"],
-        aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"],
-    )
+# dataframe: pandas dataframe to save as CSV (locally or in AWS bucket)
+# directory: folder to save the CSV file
+# filename: name of the CSV file (ending with .csv)
+# output_loc: one of 'local', 'AWS'
+# s3_bucket: S3.Bucket object where the CSV file is to be pushed
+def output_csv(
+    dataframe: pd.DataFrame,
+    directory: str,
+    filename: str,
+    output_loc: str = "local",
+    s3_bucket: Bucket = None,
+):
+    savepath = directory + filename
+    if output_loc == "local":
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        dataframe.to_csv(savepath, encoding="utf-8")
+    elif output_loc == "AWS":
+        if s3_bucket:
+            try:
+                # upload dataframe to S3 bucket
+                csv_buffer = StringIO()
+                dataframe.to_csv(csv_buffer, encoding="utf-8")
+                s3_bucket.put_object(Body=csv_buffer.getvalue(), Key=savepath)
+            except Exception:
+                print("Failed to upload data in " + savepath, file=sys.stderr)
+            else:
+                print("Uploaded dataframe as csv file " + savepath + " to bucket!")
+        else:
+            print("When using output='AWS', specify S3.Bucket object in s3_bucket.")
--- weather_data.py
+++ weather_data.py
@@ -1,18 +1,16 @@
-import sys
 import os
-from io import StringIO
 
 import pandas as pd
 import json
+from io import BytesIO
 import datetime
 import dateutil.parser
 import time
-
 import boto3
 import schedule
 
 from dotenv import load_dotenv
-from helper import get_req_handler, initBoto3Session
+from helper import initBoto3Session, get_s3_objs, get_req_handler, output_csv
 from operator import itemgetter
 
 ############ CONFIG INIT BOILERPLATE ############
@@ -34,8 +32,34 @@ def formatted_timestamp(timestamp):
     return dateutil.parser.parse(timestamp).strftime("%Y-%m-%dT%H%M%S")
 
 
-def real_time_weather(output="local"):
-    api_domain = WEATHER_API_URL
+def merge_bucket_csvs(bucket_dir, file_prefix, key_cols):
+    # merge CSV files stored in Bucket (produced for each timestamp) into a single one
+    search_key = bucket_dir + file_prefix
+    res = get_s3_objs(data_bucket, search_key)
+    merged_df = pd.DataFrame()
+    for filename, stream_data in res.items():
+        print(filename)
+        tmp_df = pd.read_csv(BytesIO(stream_data.read()), index_col=0)
+        if merged_df.empty:
+            merged_df = tmp_df.copy()
+        else:
+            merged_df = pd.concat([merged_df, tmp_df], ignore_index=True)
+    # merged dataframe may have duplicates rows (which have same values on the key_cols), these are dropped here
+    merged_df = merged_df.drop_duplicates(
+        subset=key_cols, keep="first", ignore_index=True
+    )
+
+    output_csv(
+        merged_df,
+        bucket_dir,
+        file_prefix + "_merged.csv",
+        output_loc="AWS",
+        s3_bucket=data_bucket,
+    )
+    # TODO: delete the individual csvs programmatically after the merge? if so, how to do that?
+
+
+def real_time_weather(output_loc="local"):
     api_endpoints = {
         "air_temp": "air-temperature",  # 5 stations
         "rainfall": "rainfall",  # 67 stations
@@ -47,123 +71,195 @@ def real_time_weather(output="local"):
     call_timestamp = datetime.datetime.now()
     params = {"datetime": call_timestamp}
 
-    rainfall_df = pd.DataFrame()
-    non_rainfall_df = pd.DataFrame()
+    R_df = pd.DataFrame()
+    NR_df = pd.DataFrame()
     for (weather_field, endpoint_url) in api_endpoints.items():
-        print("Calling " + weather_field + " endpoint...")
-        target_url = api_domain + endpoint_url
-        print("Sending request to " + target_url + " ...")
+        # print("Calling " + weather_field + " endpoint...")
+        target_url = WEATHER_API_URL + endpoint_url
+        # print("Sending request to " + target_url + " ...")
         resp = get_req_handler(target_url, params)
 
         resp_content = json.loads(resp.content.decode("utf-8"))
-        readings = resp_content["items"][0]["readings"]
-        stations = resp_content["metadata"]["stations"]
-        reading_frame = pd.DataFrame(
-            {
-                weather_field + "_realtime": [r["value"] for r in readings],
-                "station_id": [r["station_id"] for r in readings],
-            }
-        )
-        reading_frame.station_id = reading_frame.station_id.astype(str)
-        stat_frame = pd.DataFrame(
-            {
-                "station_id": [s["id"] for s in stations],
-                "station_loc": [
-                    (s["location"]["latitude"], s["location"]["longitude"])
-                    for s in stations
-                ],
-                "station_name": [s["name"] for s in stations],
-            }
-        )
-        stat_frame.station_id = stat_frame.station_id.astype(str)
-        field_df = pd.merge(reading_frame, stat_frame, how="outer", on="station_id")
 
+        readings = resp_content["items"][0]["readings"]
+        reading_dict = {weather_field + "_realtime": [], "station_id": []}
+        for r in readings:
+            reading_dict[weather_field + "_realtime"].append(r["value"])
+            reading_dict["station_id"].append(r["station_id"])
+        reading_frame = pd.DataFrame(reading_dict)
+        reading_frame.station_id = reading_frame.station_id.astype(str)
+
+        stations = resp_content["metadata"]["stations"]
+        station_dict = {
+            "station_id": [],
+            "station_loc": [],
+            "station_name": [],
+        }
+        for s in stations:
+            station_dict["station_id"].append(s["id"])
+            station_dict["station_loc"].append(
+                (s["location"]["latitude"], s["location"]["longitude"])
+            )
+            station_dict["station_name"].append(s["name"])
+        stat_frame = pd.DataFrame(station_dict)
+        stat_frame.station_id = stat_frame.station_id.astype(str)
+
+        field_df = pd.merge(reading_frame, stat_frame, how="outer", on="station_id")
         field_df["timestamp"] = formatted_timestamp(
             resp_content["items"][0]["timestamp"]
         )
+
         if weather_field == "rainfall":
-            rainfall_df = field_df.copy()
+            R_df = field_df.copy()
         else:
-            if non_rainfall_df.empty:
-                non_rainfall_df = field_df.copy()
+            if NR_df.empty:
+                NR_df = field_df.copy()
             else:
-                non_rainfall_df = pd.merge(
+                NR_df = pd.merge(
                     field_df.copy(),
-                    non_rainfall_df,
+                    NR_df,
                     how="inner",
                     on=["station_id", "timestamp"],
                     suffixes=("_remove", ""),
                 )
-                non_rainfall_df.drop(
-                    [c for c in non_rainfall_df.columns if "remove" in c],
+                NR_df.drop(
+                    [c for c in NR_df.columns if "remove" in c],
                     axis=1,
                     inplace=True,
                 )
 
-        print("Collected info into dataframe!")
+        # print("Collected info into dataframe!")
 
     R_col_order = (
         ["timestamp"]
-        + [c for c in rainfall_df.columns if "station" in c]
-        + [c for c in rainfall_df.columns if "_realtime" in c]
+        + [c for c in R_df.columns if "station" in c]
+        + [c for c in R_df.columns if "_realtime" in c]
     )
     NR_col_order = (
         ["timestamp"]
-        + [c for c in non_rainfall_df.columns if "station" in c]
-        + [c for c in non_rainfall_df.columns if "_realtime" in c]
+        + [c for c in NR_df.columns if "station" in c]
+        + [c for c in NR_df.columns if "_realtime" in c]
     )
-    rainfall_df = rainfall_df.reindex(columns=R_col_order)
-    non_rainfall_df = non_rainfall_df.reindex(columns=NR_col_order)
+    R_df = R_df.reindex(columns=R_col_order)
+    NR_df = NR_df.reindex(columns=NR_col_order)
 
     # rainfall dataframe saved to csv: rainfall-realtime+<timestamp>.csv
-    R_filename = WEATHER_DATA_DIR + (
+    R_filename = (
         "rainfall-realtime_"
-        + pd.to_datetime(rainfall_df["timestamp"]).min().strftime("%Y-%m-%dT%H%M%S")
+        + pd.to_datetime(R_df["timestamp"]).min().strftime("%Y-%m-%dT%H%M%S")
         + ".csv"
     )
     # non-rainfall dataframe saved to csv: non-rainfall-realtime+<timestamp>.csv
-    NR_filename = WEATHER_DATA_DIR + (
+    NR_filename = (
         "non-rainfall-realtime_"
-        + pd.to_datetime(non_rainfall_df["timestamp"]).min().strftime("%Y-%m-%dT%H%M%S")
+        + pd.to_datetime(NR_df["timestamp"]).min().strftime("%Y-%m-%dT%H%M%S")
         + ".csv"
     )
 
-    if output == "return":
-        return rainfall_df, non_rainfall_df
-    elif output == "local":
-        if not os.path.exists(WEATHER_DATA_DIR):
-            os.makedirs(WEATHER_DATA_DIR)
-        rainfall_df.to_csv(R_filename, encoding="utf-8")
-        non_rainfall_df.to_csv(NR_filename, encoding="utf-8")
-    elif output == "AWS":
-        try:
-            # upload rainfall dataframe to S3 bucket
-            R_csv_buffer = StringIO()
-            rainfall_df.to_csv(R_csv_buffer, encoding="utf-8")
-            data_bucket.put_object(Body=R_csv_buffer.getvalue(), Key=R_filename)
-        except Exception:
-            print("Failed to upload rainfall data in " + R_filename, file=sys.stderr)
-        else:
-            print("Uploaded rainfall data in " + R_filename + " to bucket!")
+    if output_loc == "return":
+        return R_df, NR_df
+    else:
+        output_csv(
+            R_df,
+            WEATHER_DATA_DIR,
+            R_filename,
+            output_loc=output_loc,
+            s3_bucket=data_bucket,
+        )
+        output_csv(
+            NR_df,
+            WEATHER_DATA_DIR,
+            NR_filename,
+            output_loc=output_loc,
+            s3_bucket=data_bucket,
+        )
 
-        try:
-            # upload non-rainfall dataframe to S3 bucket
-            NR_csv_buffer = StringIO()
-            non_rainfall_df.to_csv(NR_csv_buffer, encoding="utf-8")
-            data_bucket.put_object(Body=NR_csv_buffer.getvalue(), Key=NR_filename)
-        except Exception:
-            print(
-                "Failed to upload non-rainfall data in " + NR_filename, file=sys.stderr
-            )
-        else:
-            print("Uploaded non-rainfall data in " + NR_filename + " to bucket!")
+
+def forecast_weather_2HR(output_loc="local"):
+    # collect 2-hour-forecast data:
+    call_timestamp = datetime.datetime.now()
+    params = {"datetime": call_timestamp}
+
+    forecast_field = "forecast_2hr"
+    endpoint_url = "2-hour-weather-forecast"
+    # print("Calling " + forecast_field + " endpoint...")
+    target_url = WEATHER_API_URL + endpoint_url
+    # print("Sending request to " + target_url + " ...")
+    resp = get_req_handler(target_url, params)
+    resp_content = json.loads(resp.content.decode("utf-8"))
+
+    resp_items = resp_content["items"][0]
+    forecasts, valid_period = itemgetter("forecasts", "valid_period")(resp_items)
+
+    valid_start, valid_end = formatted_timestamp(
+        valid_period["start"]
+    ), formatted_timestamp(valid_period["end"])
+
+    forecast_dict = {"forecast_2hr_area": [], "forecast_2hr_value": []}
+    for fr in forecasts:
+        forecast_dict["forecast_2hr_value"].append(fr["forecast"])
+        forecast_dict["forecast_2hr_area"].append(fr["area"])
+    forecast_frame = pd.DataFrame(forecast_dict)
+    forecast_frame.forecast_2hr_area = forecast_frame.forecast_2hr_area.astype(str)
+
+    resp_area = resp_content["area_metadata"]
+
+    area_dict = {"forecast_2hr_area": [], "forecast_2hr_area_loc": []}
+    for ar in resp_area:
+        area_dict["forecast_2hr_area"].append(ar["name"])
+        area_dict["forecast_2hr_area_loc"].append(
+            (ar["label_location"]["latitude"], ar["label_location"]["longitude"])
+        )
+    area_frame = pd.DataFrame(area_dict)
+
+    area_frame.forecast_2hr_area = area_frame.forecast_2hr_area.astype(str)
+
+    forecast_2HR_frame = pd.merge(area_frame, forecast_frame, on="forecast_2hr_area")
+
+    forecast_2HR_frame["2hr_start"] = valid_start
+    forecast_2HR_frame["2hr_end"] = valid_end
+
+    # filename based on start of valid period for the forecast (example: 143000 -> 163000)
+    forecast_2HR_filename = "forecast-2HR_" + valid_start + ".csv"
+
+    # output frames
+    if output_loc == "return":
+        return forecast_2HR_frame
+    else:
+        output_csv(
+            forecast_2HR_frame,
+            WEATHER_DATA_DIR,
+            forecast_2HR_filename,
+            output_loc=output_loc,
+            s3_bucket=data_bucket,
+        )
 
 
 # scheduling weather data ingestion tasks:
 
-schedule.every(10).minutes.do(real_time_weather, output="AWS")
-print("Init scheduler to run ingest job every 5 min")
+RT_SCHEDULE_FREQ = 10
+schedule.every(RT_SCHEDULE_FREQ).minutes.do(real_time_weather, output_loc="AWS")
+print(
+    f"Init scheduler to run real time weather data ingest job every {RT_SCHEDULE_FREQ} mins"
+)
+
+FR_2HR_SCHEDULE_FREQ = 30
+schedule.every(FR_2HR_SCHEDULE_FREQ).minutes.do(forecast_weather_2HR, output_loc="AWS")
+print(
+    f"Init scheduler to run 2hr weather forecast data ingest job every {FR_2HR_SCHEDULE_FREQ} mins"
+)
 
 while True:
     schedule.run_pending()
     time.sleep(1)
+
+
+# one-time calls, uncomment and run as needed
+
+# real_time_weather(output_loc="AWS")
+# forecast_weather_2HR(output_loc="AWS")
+# merge_bucket_csvs(WEATHER_DATA_DIR, "forecast-2HR", ["forecast_2hr_area", "2hr_start"])
+# merge_bucket_csvs(WEATHER_DATA_DIR, "rainfall-realtime", ["timestamp", "station_id"])
+# merge_bucket_csvs(
+#     WEATHER_DATA_DIR, "non-rainfall-realtime", ["timestamp", "station_id"]
+# )
