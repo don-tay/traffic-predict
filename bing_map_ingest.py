43e792981231035ef118a06ebc7918ddc06c65c8
Retr0Metal98 <66635436+Retr0Metal98@users.noreply.github.com>
2022-04-04 15:40:51 +0800

    separate function to produce route data to use
    function `produce_route_table` converts:
    sheet consisting of 1 row per camera
    into
    CSV with rows of camera, direction (multiple rows per camera)
    
    sample of this route table in route_data.csv (actual route_data.csv should be pushed to S3 bucket).
    
    `process_route_congestion` would takes the route table from S3 (for now generates route table in memory) and sequentially calls the Bing Routes API on each row (takes 50 sec for 155 rows). these calls could potentially be parallelized to speed up.

--- bing_map_ingest.py
+++ bing_map_ingest.py
@@ -1,10 +1,11 @@
 import os
-from pprint import pprint
-import re
 
 import pandas as pd
 import json
+from io import StringIO
 import datetime
+from pprint import pprint
+import re
 
 import boto3
 from dotenv import load_dotenv
@@ -101,30 +102,8 @@ def call_route_API(
         )
 
 
-# function to call the traffic incident API, returns response for incidents within a bounding box
-def call_traffic_API(start_loc: tuple, finish_loc: tuple, incident_type: list = [2]):
-    bbox = [
-        min(start_loc[0], finish_loc[0]),  # South Latitude
-        min(start_loc[1], finish_loc[1]),  # West Longitude
-        max(start_loc[0], finish_loc[0]),  # North Latitude
-        max(start_loc[1], finish_loc[1]),  # East Longitude
-    ]
-
-    print(f"{bbox=}")
-    traffic_call_params = {
-        "type": ",".join([str(i) for i in incident_type]),
-        "key": BING_API_KEY,
-    }
-
-    resp = get_req_handler(
-        BING_TRAFFIC_URL + "/" + ",".join((str(L) for L in bbox)), traffic_call_params
-    )
-    resp_content = json.loads(resp.content.decode("utf-8"))
-    pprint(resp_content)
-
-
-def process_camera_routes(output_loc="local", call_timestamp=getCurrentDateTime()):
-    # TODO: check whether to save the location CSV in bucket or in local directory?
+def produce_route_table(output_loc="local"):
+    # run this while keeping camera_dir_locs.csv in the same directory
     dir_loc_df = pd.read_csv("camera_dir_locs.csv")
 
     common_cols = [c for c in dir_loc_df.columns if not c.startswith("dir")]
@@ -153,12 +132,34 @@ def process_camera_routes(output_loc="local", call_timestamp=getCurrentDateTime(
     route_data["dir_start"] = route_data["dir_start"].map(loc_string_to_tuple)
     route_data["dir_finish"] = route_data["dir_finish"].map(loc_string_to_tuple)
 
-    print("Route dataframe:")
-    print(route_data)
-    print("---------------------------------------------------------------------")
+    if output_loc == "return":
+        return route_data
+    else:
+        route_data_filename = "route_data.csv"
+        output_csv(
+            route_data,
+            BING_DATA_DIR,
+            route_data_filename,
+            output_loc=output_loc,
+            s3_bucket=data_bucket,
+        )
+
+
+def process_route_congestion(output_loc="local", call_timestamp=getCurrentDateTime()):
+    # temporary arrangement, should run produce_route_table() once with output_loc="AWS" before using this function.
+    route_data = produce_route_table(output_loc="return")
+    # # get route_data.csv from S3
+    # csv_obj = data_bucket.get_object(
+    #     Bucket=data_bucket.name, Key=BING_DATA_DIR + "route_data.csv"
+    # )
+    # csv_body = csv_obj["Body"]
+    # csv_string = csv_body.read().decode("utf-8")
+    # route_data = pd.read_csv(StringIO(csv_string))
+
     congestion_frames = []
     print(f"Calling Bing API for {route_data.shape[0]} routes at", getCurrentDateTime())
     for row in route_data.itertuples(index=False):
+        # collect dataframe for each route
         congestion_frames.append(
             call_route_API(
                 row.camera_id,
@@ -191,7 +192,28 @@ def process_camera_routes(output_loc="local", call_timestamp=getCurrentDateTime(
             output_loc=output_loc,
             s3_bucket=data_bucket,
         )
-    # combined_congestion_data.to_csv("combined_congestion_data.csv")
+
+
+# function to call the traffic incident API, returns response for incidents within a bounding box
+def call_traffic_API(start_loc: tuple, finish_loc: tuple, incident_type: list = [2]):
+    bbox = [
+        min(start_loc[0], finish_loc[0]),  # South Latitude
+        min(start_loc[1], finish_loc[1]),  # West Longitude
+        max(start_loc[0], finish_loc[0]),  # North Latitude
+        max(start_loc[1], finish_loc[1]),  # East Longitude
+    ]
+
+    print(f"{bbox=}")
+    traffic_call_params = {
+        "type": ",".join([str(i) for i in incident_type]),
+        "key": BING_API_KEY,
+    }
+
+    resp = get_req_handler(
+        BING_TRAFFIC_URL + "/" + ",".join((str(L) for L in bbox)), traffic_call_params
+    )
+    resp_content = json.loads(resp.content.decode("utf-8"))
+    pprint(resp_content)
 
 
 ### SAMPLE TESTS
@@ -232,4 +254,4 @@ test = {"start_loc": (1.329665, 103.842742), "finish_loc": (1.329716, 103.843949
 # print("Traffic Incident API call:")
 # call_traffic_API(test["start_loc"], test["finish_loc"])
 
-# process_camera_routes(output_loc="local")
+# process_route_congestion(output_loc="local")
