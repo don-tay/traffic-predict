04ee80057af16ad4f4c8db44d076f471a8271d3f
Retr0Metal98 <66635436+Retr0Metal98@users.noreply.github.com>
2022-04-05 16:55:51 +0800

    correction to camera_dir_locs.csv

--- task_schedule.py
+++ task_schedule.py
@@ -8,16 +8,14 @@ from bing_map_ingest import merge_bing_csvs, process_route_congestion
 # TODO: refactor output_loc to env vars
 def ingest_all(output_loc="AWS"):
     call_timestamp = getCurrentDateTime()
-    print(
-        "Calling all weather data endpoints with date_time parameter:", call_timestamp
-    )
+    print("Calling all data endpoints with date_time parameter:", call_timestamp)
 
     ingest_image(call_timestamp=call_timestamp)
     real_time_weather(output_loc=output_loc, call_timestamp=call_timestamp)
     forecast_weather_2HR(output_loc=output_loc, call_timestamp=call_timestamp)
     forecast_weather_24HR(output_loc=output_loc, call_timestamp=call_timestamp)
     forecast_weather_4DAY(output_loc=output_loc, call_timestamp=call_timestamp)
-
+    print("Completed weather data upload to S3 at", getCurrentDateTime())
     # collect congestion info at same time as image and forecast
     # may present an issue: 179 calls per 5 mins = 51552 calls per day > 50000 call limit per 24 hr
     # can try multiple API keys?
